95d60c3012a070f807013ab8c0442712c1b76ce5
王友好 <3320674424@qq.com>
2026-01-18 10:19:39 +0800

    docs: 更新配置文件中的密钥格式
    - 添加 deepseek 对象，并包含 key 属性，用于存储 Deepseek 的密钥。
    - 修正了 bot 对象中 clientSecret 属性后面缺少逗号的问题。
    - 修正了 proxy 对象中 port 属性后面缺少逗号的问题。
    - 修正了 websocket 对象中 properties 下的 $device 属性后面缺少逗号的问题。
    - 调整了 websocket 对象 intents 和 shard 属性的格式，使其更具可读性。
    - 修正了文件末尾缺少换行符的问题。

--- config.js
+++ config.js
@@ -1,21 +1,25 @@
 module.exports = {
   bot: {
     appId: "102818285",
-    clientSecret: "79BDFILORUXaeimquy27CHMRWbgmsy4A"
+    clientSecret: "79BDFILORUXaeimquy27CHMRWbgmsy4A",
   },
-  
+
+  deepseek: {
+    key: "<KEY>",
+  },
+
   proxy: {
     hostname: "118.25.65.235",
-    port: 80
+    port: 80,
   },
-  
+
   websocket: {
     intents: (1 << 25) | (1 << 26) | (1 << 27),
     shard: [0, 1],
     properties: {
       $os: process.platform,
       $browser: "my_qq_bot",
-      $device: "my_qq_bot"
-    }
-  }
+      $device: "my_qq_bot",
+    },
+  },
 };
